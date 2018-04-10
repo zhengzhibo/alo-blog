6db6ff9053f716201743653d5530361b93140f15
Zheng Zhibo <zhibo90@gmail.com>
2018-04-10 16:04:00 +0800

    add auth middleware

---
+++ client/pages/posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Post
+      </h1>
+      <h2>
+        Just a Post Page!
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
