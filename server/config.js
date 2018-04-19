e59285554771819b83ff6d30a5dbf6cd05ee182c
Alex Zheng <zhibo90@gmail.com>
2018-04-19 21:35:25 +0800

    add outset config json

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const fs = require("fs");
+const _ = require('lodash');
+
+let config = {
     app: {
         secret: 'some--secret',
         tokenExpire: '3h',
@@ -9,3 +12,19 @@ module.exports = {
         uri: './db/db.json'
     }
 }
+
+let configFile = process.env.ALO_CONFIG;
+
+if(configFile){
+    try {
+      let configJson = fs.readFileSync(configFile, 'utf8');
+      let loaded = JSON.parse(configJson);
+
+      _.merge(config, loaded);
+    } catch (error) {
+        console.error("config file not found")
+    }
+    
+
+}
+module.exports = config;
