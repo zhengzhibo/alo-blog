c957f6d348e68dfcf30e651c3eda2db9760dacf5
Zheng Zhibo <zhibo90@gmail.com>
2018-04-19 14:11:29 +0800

    fix header nav bug

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -2,37 +2,45 @@
   <header>
     <nav class="navbar">
       <span class="brace">{</span>
-      <nuxt-link to="/" class="navbar-item">HOME</nuxt-link>
-      <nuxt-link to="/about" class="navbar-item">ABOUT</nuxt-link>
-      <nuxt-link to="/posts" class="navbar-item">POSTS</nuxt-link>
-      <a href="https://github.com/zhengzhibo" target="_blank" class="navbar-item">GITHUB</a>
+      <nuxt-link to="/" :class="activeClass('index')">HOME</nuxt-link>
+      <nuxt-link to="/about" :class="activeClass('about')">ABOUT</nuxt-link>
+      <nuxt-link to="/tags" :class="activeClass('tags')">TAGS</nuxt-link>
+      <a href="https://github.com/zhengzhibo" target="_blank">GITHUB</a>
       <span class="brace">}</span>
     </nav>
     <h1>Lonely Rail</h1>
   </header>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    activeClass(name) {
+      if (this.$route.name == name) {
+        return "active";
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
 h1 {
   margin: 20px 0 40px 0;
 }
-.navbar-item {
+.navbar a {
   font-size: 1em;
   text-transform: uppercase;
   margin: 0 0.333em;
   color: #455a64;
 }
 
+.navbar a.active {
+  font-weight: 700;
+}
+
 .brace {
   font-size: 1.2em;
   font-weight: 500;
-}
-
-.nuxt-link-exact-active {
-  font-weight: 700;
 }
 </style>
 
